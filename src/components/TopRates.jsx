d172e4dc0f76c5432a283e52b32e98ad9c0f0a61
SimThT <simon.tht@outlook.com>
2023-06-16 14:11:01 +0200

    Change top rate slide functioning

--- src/components/TopRates.jsx
+++ src/components/TopRates.jsx
@@ -27,78 +27,81 @@ function TopRates() {
 
   return (
     <div className="top-rates-container">
-      <div className="left-arrow-rate" onClick={() => handleArrowClick("left")}>
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-          <g id="SVGRepo_tracerCarrier" />
-          <g id="SVGRepo_iconCarrier">
-            <g data-name="Layer 2">
-              <g data-name="arrow-ios-back">
-                <rect
-                  width="24"
-                  height="24"
-                  transform="rotate(90 12 12)"
-                  opacity="0"
-                />
-                <path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
-              </g>
+      <svg
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        className="left-arrow-rate"
+        onClick={() => handleArrowClick("left")}
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+        <g id="SVGRepo_tracerCarrier" />
+        <g id="SVGRepo_iconCarrier">
+          <g data-name="Layer 2">
+            <g data-name="arrow-ios-back">
+              <rect
+                width="24"
+                height="24"
+                transform="rotate(90 12 12)"
+                opacity="0"
+              />
+              <path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
             </g>
           </g>
-        </svg>
-      </div>
-      <div className="top-rate-boxes-wrap">
-        <div
-          className="top-rate-boxes-container"
-          style={{ transform: `translate(-${rateIndex * 110}%)` }}
-        >
-          {reviews.map((review, i) => (
-            <div className="top-rate-box" key={i}>
-              <div>
-                {Array(5)
-                  .fill()
-                  .map((_, index) => (
-                    <svg
-                      key={index}
-                      className="stars"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-                    </svg>
-                  ))}
-              </div>
-              <p>"{review.text}"</p>
-              <small>
-                &#x2015; <b>{review.name}</b> <i>{review.location}</i>
-              </small>
-            </div>
-          ))}
-        </div>
-      </div>
+        </g>
+      </svg>
       <div
+        className="top-rate-boxes-container"
+        style={{ transform: `translate(-${rateIndex * 110}%)` }}
+      >
+        {reviews.map((review, i) => (
+          <div className="top-rate-box" key={i}>
+            <div>
+              {Array(5)
+                .fill()
+                .map((_, index) => (
+                  <svg
+                    key={index}
+                    className="stars"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+                  </svg>
+                ))}
+            </div>
+            <p>"{review.text}"</p>
+            <small>
+              &#x2015; <b>{review.name}</b> <i>{review.location}</i>
+            </small>
+          </div>
+        ))}
+      </div>
+
+      <svg
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
         className="right-arrow-rate"
         onClick={() => handleArrowClick("right")}
       >
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_bgCarrier" />
-          <g id="SVGRepo_tracerCarrier" />
-          <g id="SVGRepo_iconCarrier">
-            <g data-name="Layer 2">
-              <g data-name="arrow-ios-forward">
-                <rect
-                  width="24"
-                  height="24"
-                  transform="rotate(-90 12 12)"
-                  opacity="0"
-                />
-                <path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
-              </g>
+        <g id="SVGRepo_bgCarrier" />
+        <g id="SVGRepo_tracerCarrier" />
+        <g id="SVGRepo_iconCarrier">
+          <g data-name="Layer 2">
+            <g data-name="arrow-ios-forward">
+              <rect
+                width="24"
+                height="24"
+                transform="rotate(-90 12 12)"
+                opacity="0"
+              />
+              <path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
             </g>
           </g>
-        </svg>
-      </div>
+        </g>
+      </svg>
+
       <div className="dot-selector">
         {[...Array(numberOfReviews)].map((e, i) => (
           <div
