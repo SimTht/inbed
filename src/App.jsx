483f4c77863cd0f76fd2aae4512e276d4846b6ac
SimThT <simon.tht@outlook.com>
2023-06-17 20:26:37 +0200

    Change navigation behavior to top page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import InfoContact from "./pages/InfoContact";
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollTo from "./components/ScrollTo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ScrollTo>
+        <App />
+      </ScrollTo>
     </Router>
   </React.StrictMode>
 );
